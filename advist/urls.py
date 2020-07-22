3beeb5b6a8bcc969ded46f3d4c44a242cb8939f6
savage <nickolay.pylypenko@gmail.com>
2020-07-22 10:54:51 +0300

    Created rubrics

--- advist/urls.py
+++ advist/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from .views import index, by_rubric
 app_name = 'advist'
 urlpatterns = [
-    path('', views.index, name='advist_list'),
+    path('', views.index, name='index'),
+    path('<int:rubric.id>/', views.by_rubric, name='by_rubric'),
 ]
 
--- advist/views.py
+++ advist/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Advistments
+from .models import Advistments, Rubric
 # Create your views here.
 
 
 def index(request):
     adv_list = Advistments.objects.all()
-    return render(request,'advist/index.html', {'adv_list' : adv_list})
+    rubrics = Rubric.objects.all()
+    return render(request,'advist/index.html', {'adv_list' : adv_list,
+                                                'rubrics' : rubrics})
+
+def by_rubric(request, rubric_id):
+    adv_list = Advistments.objects.filter(rubric=rubric_id)
+    rubrics = Rubric.objects.all()
+    current_rubric = Rubric.objects.get(pk=rubric_id)
+    context = {'adv_list' : adv_list, 'rubrics' : rubrics,
+               'current_rubric' : current_rubric}
+    return render(request, 'advist/by_rubric.html', context)
 #створена модель та головна сторінка із використанням джинджи
+#створена модель рубрик, шаблон, урл та контроллео, преглянути це все ще раз
